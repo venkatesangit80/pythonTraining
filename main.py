9080fa36d1b04eff0b9b6d38b097368a68ab1b2a
venkatesangit80 <venkatesangit80@gmail.com>
2024-06-17 08:04:20 -0500

    Basic Code Addition

---
+++ main.py
@@ -0,0 +1,45 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print("Hello, World!")
+
+    name = "Alice"
+    age = 25
+    height = 5.4
+
+    # Integer
+    x = 10
+    print(type(x))  # Output: <class 'int'>
+    # Float
+    y = 10.5
+    print(type(y))  # Output: <class 'float'>
+    # String
+    name = "Alice"
+    print(type(name))  # Output: <class 'str'>
+    # Boolean
+    is_student = True
+    print(type(is_student))  # Output: <class 'bool'>
+
+
+    a = 10
+    b = 5
+    print(a + b)  # Addition
+    print(a - b)  # Subtraction
+    print(a * b)  # Multiplication
+    print(a / b)  # Division
+    print(a % b)  # Modulus
+
+    name = input("Enter your name: ")
+    print("Hello, " + name + "!")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
